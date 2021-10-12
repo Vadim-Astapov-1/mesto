23f40c390880a31dff772ad957a5713b473f65ae
Vadim Astapov <mr.chibichan@yandex.ru>
2021-10-12 21:20:55 +0200

    delete debugger

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,6 @@ import Section from '../components/Section.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
 import UserInfo from '../components/UserInfo.js';
-debugger
-console.log(PopupWithImage);
 
 const editPopup = document.querySelector('.popup_type_edit');
 const editButton = document.querySelector('.profile__edit-button');
